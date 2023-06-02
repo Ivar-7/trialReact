213e467af73d715ed80c93bc5fd854477c7d919c
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-06-03 01:13:19 +0300

    bye

--- src/AuthForm.js
+++ src/AuthForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import firebase from "firebase/app";
 import "firebase/auth";
 
-const AuthForm = () => {
+function AuthForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
@@ -59,6 +59,6 @@ const AuthForm = () => {
       </form>
     </div>
   );
-};
+}
 
 export default AuthForm;
