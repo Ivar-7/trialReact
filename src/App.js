7a98924bc76cf0eb67dc67b8803343b37709976e
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-06-03 01:15:59 +0300

    removed chatRoom

--- src/App.js
+++ src/App.js
@@ -1,50 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { auth } from "./firebaseConfig";
-import "firebase/auth";
-
-// Import your custom components
-import Header from "./components/Header";
-import ChatRoomList from "./components/ChatRoomList";
-import ChatRoom from "./components/ChatRoom";
-import AuthForm from "./components/AuthForm";
+import React from "react";
 
 const App = () => {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    // Check if user is already authenticated
-    auth.onAuthStateChanged((user) => {
-      if (user) {
-        setUser(user);
-      } else {
-        setUser(null);
-      }
-    });
-  }, []);
-
-  return (
-    <Router>
-      <div className="app">
-        <Header user={user} />
-
-        <div className="container">
-          <Routes>
-            <Route
-              exact
-              path="/"
-              element={user ? <ChatRoomList user={user} /> : <AuthForm />}
-            />
-
-            <Route
-              path="/chatroom/:id"
-              element={user ? <ChatRoom user={user} /> : <AuthForm />}
-            />
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  );
+  return <div>App</div>;
 };
 
 export default App;
